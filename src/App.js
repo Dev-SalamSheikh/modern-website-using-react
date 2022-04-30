961f3639798a401371ed1009d5d52bee72d2db65
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-05-01 00:48:55 +0600

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Brand, CTA, Navbar } from "./components";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGP3,
+} from "./containers";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGP3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Article } from "../../components";
+import "./blog.css";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
+
+const Blog = () => {
+  return (
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A Lot is happening , We are Blogging About It
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article imageURL={blog01} />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          <Article
+            imageURL={blog02}
+            date="Sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imageURL={blog03}
+            date="Sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imageURL={blog04}
+            date="Sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+          <Article
+            imageURL={blog05}
+            date="Sep 26, 2021"
+            text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
